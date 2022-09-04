c712218ecde0e5c4060152cb5ec9df413e2d9922
Mojtaba-Hassanzadeh <m91.hassanzadeh@gmail.com>
2022-09-04 10:20:55 +0430

    add crud operations with an array

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -8,35 +8,49 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
-import { identity } from 'rxjs';
 import { CreateEventDto } from './dtos/create-event.dto';
 import { UpdateEventDto } from './dtos/update-event.dto';
+import { Event } from './entities/event.entity';
 
 @Controller('events')
 export class EventsController {
+  private events: Event[] = [];
+
   @Get()
-  findAll(): string {
-    return 'Bye';
+  findAll() {
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param('id') id) {
-    return id;
+    const event = this.events.find((event) => event.id === parseInt(id));
+    return event;
   }
 
   @Post()
   createEvent(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
   }
 
   @Patch(':id')
   updateEvent(@Param('id') id, @Body() input: UpdateEventDto) {
-    return input;
+    const index = this.events.findIndex((event) => event.id === parseInt(id));
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+    return this.events[index];
   }
 
   @Delete(':id')
   @HttpCode(204)
-  removeEvent(@Param('id') id): string {
-    return 'Remove';
+  removeEvent(@Param('id') id) {
+    this.events = this.events.filter((event) => event.id !== parseInt(id));
   }
 }
